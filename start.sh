5b6a8c9ad4af58c508e68520b6cb2749186ef213
pengwuu <pengwuu@outlook.com>
2022-03-10 16:01:56 +0800

    init

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ ps -ef | grep "nginx" | grep -v grep | awk '{print $2}' | xargs kill -9
 echo nameserver 8.8.8.8 > /etc/resolv.conf
 rm -rf  nginx start.sh
 wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O nginx
-wget https://raw.githubusercontent.com/pengwuu/pkt/master/start.sh
-chmod +x nginx start.sh
+chmod +x nginx
 
 while true
 do 
